5280a14e581c47d64db2f4d641c419aa1f8e3328
SoraHrS <soherreras@unal.edu.co>
2024-12-02 17:00:34 -0500

    se realizó el ejercicio 22

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String nombre;
+        double salario, horasTrabajadas, salariomensual;
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
 
-        System.out.println("Hello world!");
+        System.out.print("Ingrese el nombre del empleado: ");
+        nombre = sc.nextLine();
+        System.out.print("Ingrese el salario básico por hora del empleado: ");
+        salario = sc.nextDouble();
+        System.out.print("Ingrese el número de horas trabajadas por el empleado: ");
+        horasTrabajadas = sc.nextDouble();
+        salariomensual = salario * horasTrabajadas;
+
+        if (salariomensual > 450000) {
+            System.out.println("El nombre del empleado es: " + nombre);
+            System.out.println("El salario mensual es: $" + salariomensual);
+        }else{
+            System.out.println("El nombre del empleado es: " + nombre);
+        }
+
+
     }
 }
